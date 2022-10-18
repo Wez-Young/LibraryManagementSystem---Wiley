532615dc6d1f03c4d3bfbf7ac878e4958e445644
Wez <60880484+Wez-Young@users.noreply.github.com>
2022-10-18 16:52:06 +0100

    Updated main with entity scan and jpa repositories

--- LibraryProject/src/com/library/demo/LibraryManagementSystemApplication.java
+++ LibraryProject/src/com/library/demo/LibraryManagementSystemApplication.java
@@ -2,8 +2,12 @@ package com.library.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.library")
+@EntityScan(basePackages = "com.library.entities")
+@EnableJpaRepositories(basePackages = "com.library.persistence")
 public class LibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
