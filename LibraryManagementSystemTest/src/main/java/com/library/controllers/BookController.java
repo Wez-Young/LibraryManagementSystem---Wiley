0a1f192961801f5031a91fd228d1206654e8f4db
Wez <60880484+Wez-Young@users.noreply.github.com>
2022-10-18 16:04:16 +0100

    test

--- LibraryManagementSystemTest/src/main/java/com/library/controllers/BookController.java
+++
@@ -1,8 +0,0 @@
-package com.library.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class BookController {
-
-}
--- LibraryManagementSystemTest/src/main/java/com/library/model/persistence/BookDao.java
+++
@@ -1,9 +0,0 @@
-package com.library.model.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.library.entity.Employee;
-
-public interface BookDao extends JpaRepository<Employee, Integer>{
-
-}
