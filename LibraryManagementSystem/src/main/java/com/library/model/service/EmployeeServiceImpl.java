aba99ead86c5879287ab9d520407037a125940a1
sergiunegrusa <sergiunegrusa@yahoo.com>
2022-10-20 18:12:29 +0200

    Add files via upload
    Added login function for employee service

--- LibraryManagementSystem/src/main/java/com/library/model/service/EmployeeServiceImpl.java
+++ LibraryManagementSystem/src/main/java/com/library/model/service/EmployeeServiceImpl.java
@@ -4,12 +4,17 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.library.entity.Employee;
 import com.library.model.persistence.EmployeeDao;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@Autowired
 	private EmployeeDao employeeDao;
 
@@ -29,17 +34,16 @@ public class EmployeeServiceImpl implements EmployeeService {
 			int rows = employeeDao.insertEmployee(employee.getId(), employee.getName());
 			if (rows > 0)
 				return true;
-			}
-			catch(Exception ex) {
-				return false;
-			}
+		} catch (Exception ex) {
 			return false;
+		}
+		return false;
 	}
 
 	@Override
 	public boolean updateEmployee(Employee employee) {
 		Employee emp = employeeDao.findById(employee.getId()).orElse(null);
-		if(emp == null)
+		if (emp == null)
 			return false;
 		employeeDao.save(employee);
 		return true;
@@ -50,5 +54,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 		employeeDao.deleteById(id);
 
 		return true;
+	}
+
+	@Override
+	public Employee loginEmployee(Employee employee) {
+		return restTemplate.getForObject("http://localhost:8084/login/" + employee.getName(), Employee.class);
+
 	}
 }
